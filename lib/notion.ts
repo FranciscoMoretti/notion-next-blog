f8babb738341858d0254ae338e27136874dc494d
Francisco Moretti <franciscoemoretti@gmail.com>
2024-09-25 19:32:13 +0100

    using next cache

---
+++ lib/notion.ts
@@ -0,0 +1,110 @@
+import { Client } from "@notionhq/client";
+import { unstable_cache } from "next/cache";
+import { cache } from "react";
+import { PageObjectResponse, BlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
+
+export const revalidate = 3600; // revalidate the data at most every hour
+
+const databaseId = process.env.NOTION_DATABASE_ID as string;
+
+/**
+ * Returns a random integer between the specified values, inclusive.
+ * The value is no lower than `min`, and is less than or equal to `max`.
+ *
+ * @param {number} minimum - The smallest integer value that can be returned, inclusive.
+ * @param {number} maximum - The largest integer value that can be returned, inclusive.
+ * @returns {number} - A random integer between `min` and `max`, inclusive.
+ */
+function getRandomInt(minimum: number, maximum: number): number {
+  const min = Math.ceil(minimum);
+  const max = Math.floor(maximum);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
+const getDatabaseQuery = async (): Promise<PageObjectResponse[]> => {
+  const response = await notion.databases.query({
+    database_id: databaseId,
+  });
+  return response.results as PageObjectResponse[];
+};
+
+export const getDatabase = unstable_cache(
+  getDatabaseQuery,
+  ["notion_database"],
+  { tags: ["notion_database"], revalidate: 3600 }
+);
+
+export const getPageFromSlug = async (slug: string): Promise<PageObjectResponse | {}> => {
+  const databaseChildren = await getDatabase();
+  const page = databaseChildren.find(page => 
+    page.properties.Slug && 
+    'rich_text' in page.properties.Slug &&
+    page.properties.Slug.rich_text[0]?.plain_text === slug
+  );
+  return page || {};
+};
+
+type ExpandedBlockObjectResponse = BlockObjectResponse & {
+  children?: ExpandedBlockObjectResponse[];
+};
+
+
+export async function getBlocks(blockID: string) {
+  const cachedBlocks = unstable_cache(
+  (blockID: string) => getNotionBlocks(blockID),
+  [`notion_blocks_${blockID}`],
+  { tags: [`notion_blocks_${blockID}`], revalidate: 3600 }
+);
+return cachedBlocks(blockID);}
+
+async function getNotionBlocks (blockID: string): Promise<ExpandedBlockObjectResponse[]> {
+  const blockId = blockID.replaceAll("-", "");
+
+  const { results } = await notion.blocks.children.list({
+    block_id: blockId,
+    page_size: 100,
+  }) as {results: BlockObjectResponse[]};
+
+  // TODO: Use error handling to check if isFullBlock and report
+
+  const childBlocks = results.map(async (block: BlockObjectResponse) => {
+    if (block.has_children) {
+      const children = await getNotionBlocks(block.id);
+      return { ...block, children } as ExpandedBlockObjectResponse;
+    }
+    return block as ExpandedBlockObjectResponse;
+  });
+
+  return Promise.all(childBlocks).then((blocks) =>
+    blocks.reduce((acc: ExpandedBlockObjectResponse[], curr) => {
+      if (curr.type === "bulleted_list_item") {
+        if (acc[acc.length - 1]?.type === "bulleted_list") {
+          (acc[acc.length - 1] as any).bulleted_list.children?.push(curr);
+        } else {
+          acc.push({
+            id: getRandomInt(10 ** 99, 10 ** 100).toString(),
+            type: "bulleted_list",
+            bulleted_list: { children: [curr] },
+          } as ExpandedBlockObjectResponse);
+        }
+      } else if (curr.type === "numbered_list_item") {
+        if (acc[acc.length - 1]?.type === "numbered_list") {
+          (acc[acc.length - 1] as any).numbered_list.children?.push(curr);
+        } else {
+          acc.push({
+            id: getRandomInt(10 ** 99, 10 ** 100).toString(),
+            type: "numbered_list",
+            numbered_list: { children: [curr] },
+          } as ExpandedBlockObjectResponse);
+        }
+      } else {
+        acc.push(curr);
+      }
+      return acc;
+    }, [])
+  );
+}
